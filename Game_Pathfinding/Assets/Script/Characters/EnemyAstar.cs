268da086116e3f80aca57048e02479acde2c4614
Firrow <73218766+Firrow@users.noreply.github.com>
2023-10-18 17:53:20 +0200

    TODO : Character script

---
+++ Game_Pathfinding/Assets/Script/Characters/EnemyAstar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAstar : Character
+{
+    // Algo A*
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Game_Pathfinding/Assets/Script/Characters/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+
+    //Déplacement du joueur
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

c1bac6cbbac152c681943bde394497f62c67e277
Firrow <firrowmb@gmail.com>
2023-10-31 17:19:58 +0100

    AStar avec Movement Araignée

--- Game_Pathfinding/Assets/Script/Characters/EnemyAstar.cs
+++ Game_Pathfinding/Assets/Script/Characters/EnemyAstar.cs
@@ -5,268 +5,176 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
+/*
+ NOTE : L'ennemi pouvant aller en diagonal, il peut chevaucher certaines tuiles durant son déplacement
+        Cependant, uniquement la valeur de la tuile d'arrivée sera prise en compte pour la vitesse de déplacement
+ */
+
+
+
 public class EnemyAstar : MonoBehaviour
 {
-    //map
-    private MapManager mapManager;
-    [SerializeField] private Tilemap map;
+    //Autre éléments
+    private GameObject player;
     public Grid grid;
+    public int speed;
 
-    //déplacement Ennemis
-    [SerializeField] private float moveTime;
-    private float moveCounter;
-    [SerializeField] private float baseSpeed;
-
-    //AStar
-    List<Vector3Int> openList = new List<Vector3Int>();
-    List<Vector3Int> closeList = new List<Vector3Int>();
-    Dictionary<Vector3Int, Vector3Int> cameFrom = new Dictionary<Vector3Int, Vector3Int>();
+    private int cost;
+    private MapManager mapManager;
     private Vector3Int goal;
     private Vector3Int start;
     private Vector3Int current;
+    Dictionary<Vector3Int, int> openList = new Dictionary<Vector3Int, int>();
+    Dictionary<Vector3Int, int> closeList = new Dictionary<Vector3Int, int>();
+    Dictionary<Vector3Int, int> possibleTiles = new Dictionary<Vector3Int, int>();
+    Dictionary<Vector3Int, Vector3Int> cameFrom = new Dictionary<Vector3Int, Vector3Int>();
     List<Vector3Int> neighbors = new List<Vector3Int>();
-    Dictionary<Vector3Int, int> gScore = new Dictionary<Vector3Int, int>();
-    Dictionary<Vector3Int, int> hScore = new Dictionary<Vector3Int, int>();
-    Dictionary<Vector3Int, int> fScore = new Dictionary<Vector3Int, int>();
-    int index;
-    int tempFS;
-    List<float> cost = new List<float>();
-    List<Vector3Int> possibleTile = new List<Vector3Int>();
-
-    //Autre éléments
-    private GameObject player;
+    List<Vector3Int> path = new List<Vector3Int>();
+    private int currentPathIndex = 0;
 
 
     private void Awake()
     {
         mapManager = FindAnyObjectByType<MapManager>();
-        //moveCounter = moveTime;
     }
 
 
-    // Algo A*
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-        //récupérer tuile position player
-        /*goal = mapManager.GetGridCellAtWorldPosition(player.transform.position, grid);
-        //récupérer tuile position ennemi
         start = mapManager.GetGridCellAtWorldPosition(this.transform.position, grid);
-        current = start;*/
-
-        current = mapManager.GetGridCellAtWorldPosition(this.transform.position, grid);
-
-    }
-
-    [Obsolete]
-    void Update()
-    {
         //récupérer tuile position player
         goal = mapManager.GetGridCellAtWorldPosition(player.transform.position, grid);
-        List<Vector3Int> path = new List<Vector3Int>();
 
+        path = AStar(goal);
 
-        //--------------------------------------------------------------------------------------------
-        openList.Add(current);
-        gScore.Add(current, 0);
-        hScore.Add(current, Heuristique(current, goal)); //attention : est ce qu'il faut utiliser start ou current ?
-        fScore.Add(current, gScore[current] + hScore[current]);
-
-        while (openList.Count > 0)
+        foreach (var tile in path)
         {
-            Debug.Log("current : " + current);
-
-            tempFS = 10000000;
-            foreach (var fs in fScore)
-            {
-                if (fs.Value < tempFS)
-                {
-                    current = fs.Key;
-                    tempFS = fs.Value;
-                }
-            }
-
-            if (current == goal)
-            {
-                Debug.Log("FIN !");
-                path = ReconstructPath(cameFrom, current);
-            }
-
-            openList.Remove(current);
-            closeList.Add(current);
-
-            neighbors = mapManager.GetNeighbors(current);
-
-            index = 0;
-            foreach (var neighbor in neighbors)
-            {
-                index++;
-
-                if (closeList.Contains(neighbor))
-                    continue;
-
-                if (!closeList.Contains(neighbor) && !openList.Contains(neighbor)/* || tentativeGScore < gScore[neighbor]*/)
-                {
-                    //Ajout des cases voisines à la liste ouverte
-                    //gScore.Remove(neighbor);
-                    int tentativeGScore = DistanceBetween(current, neighbor);
-                    gScore.Add(neighbor, tentativeGScore);
-                    hScore.Add(neighbor, Heuristique(neighbor, goal));
-                    fScore.Add(neighbor, gScore[neighbor] + hScore[neighbor]);
-                    if (!openList.Contains(neighbor))
-                    {
-                        openList.Add(neighbor);
-                        cameFrom.Add(neighbor, current);
-                        cost.Add(mapManager.GetTileMovementSpeed(neighbor));
-                    }
-                }
-            }
-
-            //On regarde quelles tuiles ont le coup minimum et on en choisit une au hasard
-            int min = (int)cost.Min(); //PROBLEME AVEC COST MIN : la tuile n'est pas forcément accessible (si elle est dans la listClose)
-            
-            Debug.Log("cout min : " + min);
-            //cost.Sort();
-            foreach (var tile in openList)
-            {
-                if (mapManager.GetTileMovementSpeed(tile) == min)
-                {
-                    possibleTile.Add(tile);
-                }
-            }
-            /*
-             PARCOURIR TOUTES LES TUILES VOISINES :
-                SI LA TUILE N'EST PAS DANS LA LISTE CLOSE
-                    SI LA TUILE A LE COUT LE PLUS BAS :
-                        AJOUTER A LA LISTE DES TUILES POSSIBLES
-                SI TOUTES LES TUILES AVEC LE COUT LE + BAS SONT DANS LA LISTE CLOSE, LE MIN PASSE À Cost[1], etc.
-             */
-
-            int RandomIndex = UnityEngine.Random.Range(0, possibleTile.Count());
-            current = possibleTile[RandomIndex];
-            this.transform.position = current; //JUSTE POUR INFORMATION
-            gScore.Clear();
-            hScore.Clear();
-            fScore.Clear();
-            possibleTile.Clear();
-            cost.Clear();
-            neighbors.Clear();
-            Debug.Log("------------------------------------------------------------------------------------------------------");
+            Debug.Log(tile);
         }
 
-        //throw new Exception("Erreur chemin");
-
-        /*foreach (var c in path)
-        {
-            Debug.Log(c);
-        }*/
-        //Debug.Log("----------------------------");
     }
 
-    /*private void Move()
+    void Update()
     {
-        //Rotation (aléatoire)
-        moveCounter -= Time.deltaTime;
-        if (moveCounter <= 0)
+        // Vérifiez si l'ennemi a atteint la fin du chemin
+        if (currentPathIndex < path.Count)
         {
-            moveCounter = moveTime;
-            //transform.rotation = Quaternion.Euler(0f, 0f, Random.RandomRange(0f, 360f));
+            Vector3Int nextTile = path[currentPathIndex];
+            Vector3 nextTilePosition = grid.GetCellCenterWorld(nextTile);
+
+            // Vérifiez la distance entre l'ennemi et la tuile suivante
+            float distanceToNextTile = Vector3.Distance(transform.position, nextTilePosition);
+            if (distanceToNextTile < 0.1f)
+            {
+                currentPathIndex++; // Passez à la tuile suivante
+            }
+            else
+            {
+                // Déplacez l'ennemi vers la tuile suivante
+                float adjustedSpeed = speed / mapManager.GetTileMovementSpeed(transform.position);
+                Vector3 moveDirection = (nextTilePosition - transform.position).normalized;
+                transform.position += moveDirection * adjustedSpeed * Time.deltaTime; // Ajoutez la vitesse de déplacement de l'ennemi en fonction de la tuile sur laquelle il se trouve
+            }
         }
+    }
 
-        //Déplacement
-        float adjustedSpeed = baseSpeed / mapManager.GetTileMovementSpeed(transform.position + transform.up * Time.deltaTime); //on regarde la case plus loin pour ne pas être bloquée sur la case mur
-        transform.position += transform.up * Time.deltaTime * adjustedSpeed;
-    }*/
-
-    /*public List<Vector3Int> AStar(Vector3Int start, Vector3Int goal) //pas un void
+    private List<Vector3Int> AStar(Vector3Int goal)
     {
-        Debug.Log(start);
-        Debug.Log(goal);
+        //--------------------------------------------------------------------------------------------
+        //1) On ajoute la tuile de départ à la liste ouverte
+        //2) Ajouter son cout (= 0)
+        cost = 0;
+        openList.Add(start, cost);
+        current = start;
 
-        openList.Add(start);
-        //gScore.Add(start, 0);
-        hScore.Add(start, Heuristique(current, goal)); //attention : est ce qu'il faut utiliser start ou current ?
-        fScore.Add(start, gScore[start] + hScore[start]);
-
+        //3) TANT QUE LA LISTE OUVERTE N'EST PAS VIDE
         while (openList.Count > 0)
         {
-            int tempFS = 10000000;
-            foreach (var fs in fScore)
-            {
-                if (fs.Value < tempFS)
-                {
-                    current = fs.Key;
-                    tempFS = fs.Value;
-                }
-            }
-            Debug.Log("current : " + current);
+            //Debug.Log("tuile actuelle : " + current);
+            //a) si tuile actuelle est la tuile de fin --> FIN sinon
             if (current == goal)
             {
-                Debug.Log("FIN !");
+                //Debug.Log("FIN !!!");
+                openList.Clear();
                 return ReconstructPath(cameFrom, current);
             }
-
-            openList.Remove(current);
-            closeList.Add(current);
-
-            neighbors = mapManager.GetNeighbors(current);
-            gScore.Add(current, (int)mapManager.GetTileMovementSpeed(current)); //mise à jour du gScore
-
-            Debug.Log(" AVANT FOREACH ");
-            foreach (var neighbor in neighbors)
+            else
             {
-                Debug.Log("voisin : " + neighbor);
-                if (closeList.Contains(neighbor))
-                    continue;
+                //b) déplacer la tuile actuelle de la liste ouverte à la liste fermée //ATTENTION : A VOIR SI PAS D'ERREUR TOUR 1 AVEC CURRENT
+                closeList.Add(current, cost);
+                openList.Remove(current);
 
-                gScore.Add(neighbor, (int)mapManager.GetTileMovementSpeed(neighbor)); //on ajoute la valeur de la tuile voisin qu'on observe
-                int tentativeGScore = gScore[neighbor] + DistanceBetween(current, neighbor);
-
-                if (!openList.Contains(neighbor) || tentativeGScore < gScore[neighbor])
+                //c) déterminer toutes les tuiles voisines
+                neighbors = mapManager.GetNeighbors(current);
+                //d) pour toutes les tuiles voisines v :
+                foreach (var neighbor in neighbors)
                 {
-                    //Passage de la case "current" à la case voisine
-                    cameFrom.Add(neighbor, current);
-                    gScore.Add(neighbor, tentativeGScore);
-                    hScore.Add(neighbor, Heuristique(neighbor, goal));
-                    fScore.Add(neighbor, gScore[neighbor] + hScore[neighbor]);
-                    if (!openList.Contains(neighbor))
+                    //si v n'est ni dans liste ouverte, ni dans liste fermée, ni de type "mur" -->
+                    if (!openList.ContainsKey(neighbor) && !closeList.ContainsKey(neighbor) && mapManager.GetTile(neighbor).name != "wall")
                     {
-                        openList.Add(neighbor);
-                        this.transform.position = neighbor;
+                        //calculer son cout
+                        cost = DistanceBetween(neighbor, start) + DistanceBetween(neighbor, goal) + (int)mapManager.GetTileMovementSpeed(neighbor);
+
+                        //ajouter dans liste ouverte (dictionnaire (tuile, cout))
+                        openList.Add(neighbor, cost);
+                        cameFrom.Add(neighbor, current);
                     }
                 }
+                neighbors.Clear();
+
+                int minCost = 1000000;
+                //e) déterminer le cout le + bas
+                foreach (var tile in openList)
+                {
+                    if (tile.Value < minCost)
+                    {
+                        minCost = tile.Value;
+                    }
+                }
+                //f) choisir un des voisins ayant le cout le + bas pour devenir la nouvelle "current"
+                foreach (var tile in openList)
+                {
+                    if (tile.Value == minCost)
+                    {
+                        possibleTiles.Add(tile.Key, tile.Value);
+                    }
+                }
+
+                //g) choisir la nouvelle tuile actuelle
+                System.Random r = new System.Random();
+                int RandomIndex = r.Next(possibleTiles.Count);
+                current = possibleTiles.ElementAt(RandomIndex).Key;
+
+                possibleTiles.Clear();
             }
         }
 
-        throw new Exception("Erreur chemin");
-    }*/
-
-    private int Heuristique(Vector3Int current, Vector3Int goal)
-    {
-        //diapo 45 : prendre pour coup la distance entre les deux tuiles + le cout du terrain
-        int dx = Math.Abs(current.x - goal.x);
-        int dy = Math.Abs(current.y - goal.y);
-        int currentCost = (int)mapManager.GetTileMovementSpeed(current);
-
-        return dx + dy + currentCost;
+        throw new Exception("ERREUR CHEMIN IMPOSSIBLE ! ");
     }
 
-    //Distance en fonction du cout de la tuile
-    private int DistanceBetween(Vector3Int current, Vector3Int neighbor)
+    private int DistanceBetween(Vector3Int current, Vector3Int otherTile)
     {
-        int tileValueA = (int)mapManager.GetTileMovementSpeed(current);
-        int tileValueB = (int)mapManager.GetTileMovementSpeed(neighbor);
-        return Math.Abs(tileValueA - tileValueB);
+        //diapo 45 : prendre pour cout la distance entre les deux tuiles + le cout du terrain
+        int dx = Math.Abs(current.x - otherTile.x);
+        int dy = Math.Abs(current.y - otherTile.y);
+
+        return dx + dy;
     }
+
 
     private List<Vector3Int> ReconstructPath(Dictionary<Vector3Int, Vector3Int> cameFrom, Vector3Int current)
     {
         List<Vector3Int> totalPath = new List<Vector3Int>();
+        totalPath.Add(goal);
         while (cameFrom.ContainsKey(current))
         {
             current = cameFrom[current];
             totalPath.Add(current);
         }
+
+        totalPath.Reverse();
         return totalPath;
     }
 }
